691f84494db00ad199d90c378286e7b472dab3df
JeevithaSri <jeevithasri0107@gmail.com>
2021-05-29 12:23:00 -0700

    Completion Status - UI

--- src/app/Admin_Module/completion/completion.component.ts
+++ src/app/Admin_Module/completion/completion.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Manager {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-completion',
   templateUrl: './completion.component.html',
   styleUrls: ['./completion.component.scss']
 })
 export class CompletionComponent implements OnInit {
+
+  managers: Manager[] = [
+    {value: 'manager-0', viewValue: 'Manager1'},
+    {value: 'manager2-1', viewValue: 'Manager2'},
+    {value: 'manager3-2', viewValue: 'Manager3'}
+  ];
 
   constructor() { }
 
